6c7fb3e3ce5fe001418eedf836b056f78f3ef8d9
Benji Pugh <pughbenji@gmail.com>
2021-05-05 21:47:07 -0400

    Added gradient neato ascent

---
+++ make_hardcoded_gauntlet.m
@@ -0,0 +1,20 @@
+function [potential_func, grad_func] = make_hardcoded_gauntlet(point_spacing, bob_attraction_factor)
+% Hard coded values assembled into a potential function for the gauntlet
+% challenge
+
+    % create walls
+    [w_pot, w_grad] = make_rectangle(4, 4.37, [0.5, -1.185], 0, point_spacing);
+    % create box 1
+    [s1_pot, s1_grad] = make_square(0.5, [1.41, -2], 0, point_spacing);
+    % create box 2
+    [s2_pot, s2_grad] = make_square(0.5, [1, -0.7], pi/4, point_spacing);
+    % create box 3
+    [s3_pot, s3_grad] = make_square(0.5, [-0.25, -1], pi/4, point_spacing);
+    % create BoB
+    bob_pot = make_potential_BoB([0.75, -2.5], 0.25, point_spacing / bob_attraction_factor);
+    bob_grad = make_gradient_BoB([0.75, -2.5], 0.25, point_spacing / bob_attraction_factor);
+
+    potential_func = @(x, y) bob_pot(x, y) + w_pot(x, y) + s1_pot(x, y) + s2_pot(x, y) + s3_pot(x, y);
+    grad_func = @(x, y) bob_grad(x, y) + w_grad(x, y) + s1_grad(x, y) + s2_grad(x, y) + s3_grad(x, y);
+end
+
--- neato_gradient_descent.m
+++ neato_gradient_descent.m
@@ -1,2 +1,71 @@
-function neato_gradient_descent()
+function position = flatlandSolution()
+track_width = 0.235; % Track width (m)
+lambda = 1/100; % Gradient scaling factor (unitless)
 
+heading = [0; 1]; % Initial heading (unitless)
+position = [0; 0]; % Initial position (m)
+
+angular_speed = 0.1; % (rad/s)
+linear_speed = 0.05; % (m/s)
+
+pub = rospublisher('/raw_vel');
+sub_bump = rossubscriber('/bump');
+msg = rosmessage(pub);
+msg.Data = [0, 0];
+send(pub, msg);
+pause(2); % Wait for everything to settle
+
+placeNeato(position(1), position(2), heading(1), heading(2));
+pause(2); % Wait for the neato to fall
+
+% Point spacing of 0.05 (m) and BoB attraction factor of 7 (unitless)
+[~, grad_func] = make_hardcoded_gauntlet(0.05, 7);
+
+while true
+    % Gradient value at the current position
+    grad_value = -grad_func(position(1), position(2));
+    grad_value = grad_value(:);
+
+    % We'll turn for this amount of time and to this angle
+    turn_angle = atan2(grad_value(2), grad_value(1));
+    turn_time = double(turn_angle) / angular_speed;
+    turn_direction = sign(turn_angle);
+    
+    % From standard differential drive forward kinematics equations
+    msg.Data = [-turn_direction*angular_speed*track_width/2
+                turn_direction*angular_speed*track_width/2];
+    send(pub, msg);
+    
+    % Wait until we're done turning
+    rostic;
+    while rostoc < turn_time
+        pause(0.01);
+    end
+
+    % How far we'll drive straight and how long it'll take
+    forward_distance = norm(grad_value*lambda);
+    forward_time = forward_distance / linear_speed;
+    
+    % Drive straight
+    msg.Data = [linear_speed, linear_speed];
+    send(pub, msg);
+    rostic;
+    while rostoc < forward_time
+        pause(0.01)
+    end
+    
+    % Update our position at the next timestep
+    position = position + grad_value*lambda;
+    
+    % Break when at target or we bump something
+    bumpMessage = receive(sub_bump);
+    if forward_distance < 0.01 || any(bumpMessage.Data)
+        break;
+    end
+end
+
+% Don't keep driving lol
+msg.Data = [0, 0];
+send(pub, msg);
+
+end
